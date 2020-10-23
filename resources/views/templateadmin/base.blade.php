aeb41c10cdca2a69ac53a24e18c536730980477e
rcoagstan <agustianr17@gmail.com>
2020-10-23 09:11:37 +0700

    create ecommerce(login,regis,home,about,product,shopcart) dan edit base.blade.php

--- resources/views/templateadmin/base.blade.php
+++ resources/views/templateadmin/base.blade.php
@@ -9,14 +9,14 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Blank</title>
+  <title>Admin Blank Pages</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="assest/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="assest/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -33,7 +33,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Admin <sup><br>Muhammad Rico Agustian</sup></div>
       </a>
 
       <!-- Divider -->
@@ -381,14 +381,14 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assest/assest/assest/vendor/jquery/jquery.min.js"></script>
+  <script src="assest/assest/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="assest/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="assest/js/sb-admin-2.min.js"></script>
 
 </body>
 
