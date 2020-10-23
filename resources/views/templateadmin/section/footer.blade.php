aa06631a0aa4ac03ef08259b05abdc02ffbd119c
rcoagstan <agustianr17@gmail.com>
2020-10-23 09:56:35 +0700

    create footer dan update base blade

---
+++ resources/views/templateadmin/section/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="float-left d-none d-sm-inline">
+            <span>Copyright &copy;2020 <a href="https://rcoagstan.com">Rico Agustian</a> </span>
+          </div>
+          <div class="float-right d-none d-sm-inline"> Muhammad Rico Agustian
+          	
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
