5b72335fd5005bb56616c467545f219d4f377a61
rcoagstan <agustianr17@gmail.com>
2020-10-23 09:43:46 +0700

    update header dan base

--- resources/views/templateadmin/base.blade.php
+++ resources/views/templateadmin/base.blade.php
@@ -33,7 +33,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Admin <sup><br>Muhammad Rico Agustian</sup></div>
+        <div class="sidebar-brand-text mx-3">Admin <sup></sup></div>
       </a>
 
       <!-- Divider -->
