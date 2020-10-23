b1529adc1cab60f7c0fa26909b9df9dac100e847
rcoagstan <agustianr17@gmail.com>
2020-10-23 08:57:28 +0700

    update web dan create base.blade.php untuk template admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('ecommerce.home');
+});
+
+Route::get('/about', function () {
+    return view('ecommerce.about');
+});
+
+Route::get('/login', function () {
+    return view('ecommerce.login');
+});
+
+Route::get('/product', function () {
+    return view('ecommerce.product');
+});
+
+Route::get('/product-detail', function () {
+    return view('ecommerce.product-detail');
+});
+
+Route::get('/shoping-cart', function () {
+    return view('ecommerce.shoping-cart');
+});
+
+Route::get('/register', function () {
+    return view('ecommerce.register');
+});
+
+Route::get('/base', function () {
+    return view('templateadmin.base');
+});
